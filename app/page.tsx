ac390550de7d0a1fe6af2b757c7ac420f647de2d
inventramx-ux <inventramx@gmail.com>
2026-02-14 22:12:32 -0600

    finalt

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         >
           <div className="relative w-full h-[220px] sm:h-[280px]">
             <Image
-              src="/banner.png"
+              src="/$.png"
               alt="Axiom Pro"
               fill
               className="object-cover"
@@ -43,16 +43,44 @@ export default function Home() {
 
           <div className="flex flex-col items-center justify-center px-6 py-6 gap-2 bg-[#0a0a0a]">
             <span className="text-lg sm:text-xl font-bold">
-              Join Telegram
+           Telegram Public
             </span>
             <span className="text-sm text-gray-400 font-normal text-center leading-relaxed">
-              Access a Telegram channel focused on crypto.
+              Join our free crypto community with daily signals, news, and educational content. 
+             
             </span>
           </div>
         </Link>
 
         {/* Secondary button */}
         <Link
+          href="https://whop.com/checkout/plan_QDGKlwSWIj4mv"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            mt-5 w-[92%] max-w-[500px]
+            h-[90px]
+            border border-gray-800 rounded-2xl
+            flex items-center gap-4 px-5
+            hover:bg-white/5 transition-colors
+          "
+        >
+          <div className="relative w-[52px] h-[52px] rounded-lg overflow-hidden shrink-0">
+            <Image
+              src="/MP.png"
+              alt="Axiom Pro"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-base">Telegram PRO</span>
+            <span className="text-sm text-gray-400 font-normal">
+Get exclusive access to premium crypto insights, trading signals, market news, and advanced tools to elevate your trading game.           </span>
+          </div>
+        </Link>
+ <Link
           href="https://axiom.trade/@torem"
           target="_blank"
           rel="noopener noreferrer"
@@ -76,11 +104,10 @@ export default function Home() {
           <div className="flex flex-col">
             <span className="text-base">Axiom Pro</span>
             <span className="text-sm text-gray-400 font-normal">
-              Free Pro access and 10% off fees
+              Get free Pro access and save 10% on trading fees.
             </span>
           </div>
         </Link>
-
         {/* Footer */}
         <div className="mt-auto w-full flex justify-center gap-4 text-sm text-gray-400 py-6">
           <Link
